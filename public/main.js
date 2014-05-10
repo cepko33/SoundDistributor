5752cbbd79fab91539e6c654e8a49241b382b0be
Stefan Cepko <cepko33@yahoo.com>
2014-05-09 22:29:57 -0400

    added other files

---
+++ public/main.js
@@ -0,0 +1,157 @@
+function Total() {
+  var FADE_TIME = 150; // ms
+  var TYPING_TIMER_LENGTH = 400; // ms
+  var COLORS = [
+    '#e21400', '#91580f', '#f8a700', '#f78b00',
+    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+  ];
+
+  // Initialize varibles
+  var $window = $(window);
+
+  var context;
+  window.addEventListener('load', init, false);
+  function init() {
+    try {
+      // Fix up for prefixing
+      window.AudioContext = window.AudioContext||window.webkitAudioContext;
+      context = new AudioContext();
+      g = context.createGain();
+    }
+    catch(e) {
+      alert('Web Audio API is not supported in this browser');
+    }
+  }
+
+function createSource(buffer) {
+    var source = context.createBufferSource();
+    var gainNode = context.createGain();
+    source.buffer = buffer;
+    source.loop = true;
+    source.connect(gainNode);
+    gainNode.connect(context.destination);
+}
+
+
+this.play = function(freq, start, end, vol) {
+    var startTime = context.currentTime + 0.100;
+    o = context.createOscillator();
+    g = context.createGain();
+    o.frequency.value = freq;
+    o.connect(g);
+    g.connect(context.destination);
+    g.gain.value = vol;
+    o.start(startTime + start);
+    o.stop(startTime + start + end);
+}
+
+
+this.end = function(time) {
+    o.stop(time);
+}
+
+this.rhythm = function() {
+    var startTime = context.currentTime + 0.100;
+    var tempo = 120;
+    var eighthNoteTime = (60 / tempo) / 2;
+
+    for (var x = 0; x < 20; x++){
+
+        this.play(100 + x, 0 + x, .33, 0.1);
+
+        this.play(125 + x, .33 + x, .33, 0.2);
+
+        this.play(150 + x, .66 + x, .33, 0.8);
+    }
+}
+};
+
+var t = new Total();
+
+function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  var loader = this;
+
+  request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount == loader.urlList.length)
+          loader.onload(loader.bufferList);
+      },
+      function(error) {
+        console.error('decodeAudioData error', error);
+      }
+    );
+  }
+
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
+  request.send();
+}
+
+BufferLoader.prototype.load = function() {
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
+}
+
+window.onload = init;
+var context;
+var bufferLoader;
+
+function init() {
+  // Fix up prefixing
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  context = new AudioContext();
+
+  bufferLoader = new BufferLoader(
+    context,
+    [
+      './sounds/strings0.wav',
+      './sounds/strings1.wav',
+      './sounds/strings2.wav',
+    ],
+    finishedLoading
+    );
+
+  bufferLoader.load();
+}
+
+function finishedLoading(bufferList) {
+  // Create two sources and play them both together.
+  var source1 = context.createBufferSource();
+  var source2 = context.createBufferSource();
+  var source3 = context.createBufferSource();
+  source1.buffer = bufferList[0];
+  source2.buffer = bufferList[1];
+  source3.buffer = bufferList[2];
+  
+  source1.loop = true;   
+  source2.loop = true;   
+  source3.loop = true;   
+
+  source1.connect(context.destination);
+  source2.connect(context.destination);
+  source3.connect(context.destination);
+}
